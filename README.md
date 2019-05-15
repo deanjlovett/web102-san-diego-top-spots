72d7af0ab7ba28c53288a6d61edcef43224dc17c
Dean Lovett <dean.lovett@gmail.com>
2019-05-14 17:36:20 -0700

    adds code to read json and display it as an html table

---
+++ README.md
@@ -0,0 +1,3 @@
+#web102-san-diego-top-spots
+
+A simple html page to display JSON as an html table
--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 $(document).ready(function() {
 // write your code here
-
+    $.getJSON("data.json", (json)=>{
+        var sdTopSpots = '';
+        $.each(json, (key, value)=>{
+            sdTopSpots += '<tr>'
+            sdTopSpots += '<td>' + value.name + '</td>';
+            sdTopSpots += '<td>' + value.description + '</td>';
+            sdTopSpots += '<td> "https://www.google.com/maps?q=' + value.location + '"</td>';
+            sdTopSpots += '</tr>';
+        });
+        $('#san-diego-top-spots').append(sdTopSpots);
+        console.log(sdTopSpots);
+    });
 });
